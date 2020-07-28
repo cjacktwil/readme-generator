631d4ef4d1c08e96c03ddfc554f6e23d193ab284
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-07-28 18:02:51 -0500

    generated initial readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# readme-generator
+# Test
+
+  ## Description
+  ;lavnl;kanv;lasn'd
+
+  // ## Table of Contents - optional - ask question about if user wants to include, then create functions to add, if true
+
+  ## Installation
+  d;vlnasv;nsa'
+
+  ## Usage
+  dl;knvasflkvs'ad
+  // include option to add screenshots
+
+  ## Credits
+  ;lkavsvlka
+
+  ## License
+  none
+
+  //## Badges - optional
+
+  //## Contributing - optional
+
+  //## Tests - optional
+
--- index.js
+++ index.js
@@ -4,132 +4,140 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
-// const questions = [
-
-// ];
-
-// function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// function to initialize program
-// function init() {
-
-// }
-
-// prompt questions
-const promptUser = () => {
-    return inquirer.prompt ([
+const questions = [
     {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?',
-    validate: titleInput => {
-        if (titleInput) {
-            return true;
-        } else {
-            console.log('Please enter the name of your project.')
-            return false;
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your project.')
+                return false;
+            }
         }
-    }
+        },
+        {
+            type: 'confirm',
+            name: 'table',
+            message: 'Would you like to add a table of contents?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project:',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please describe your project.')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How does one install your project?'
+        },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is this application used?'
     },
     {
         type: 'confirm',
-        name: 'table',
-        message: 'Would you like to add a table of contents?',
+        name: 'screenshotConfirm',
+        message: 'Would you like to include screenshot(s)?',
+        default: false
+    },
+    {
+        type: 'link',
+        name: 'screenshot',
+        message: 'Enter the link to your screenshot.',
+        when: ({screenshotConfirm}) => screenshotConfirm
+    },
+    {
+        type: 'confirm',
+        name: 'screenshotConfirmTwo',
+        message: 'Would you like to include another screenshot?',
+        default: false,
+        when: ({screenshotConfirm}) => screenshotConfirm
+    },
+    {
+    type: 'input',
+    name: 'credits',
+    message: 'Who worked on this project and needs to be listed in the credit section?'
+    },
+    {
+        type: 'link',
+        name: 'creditLink',
+        message: 'Please enter the GitHub link for your collaborator.'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license does this project hold?'
+    },
+    {
+        type: 'confirm',
+        name: 'badgesConfirm',
+        message: 'Would you like to list any badges?',
         default: false
     },
     {
         type: 'input',
-        name: 'description',
-        message: 'Please describe your project:',
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('Please describe your project.')
-            }
-        }
+        name: 'badges',
+        message: 'What badges would you like to add?',
+        when: ({badgesConfirm}) => badgesConfirm
+    },
+    {
+        type: 'confirm',
+        name:'contributingConfirm',
+        message: 'Would you like others to contribute to this project?',
+        default: false
     },
     {
         type: 'input',
-        name: 'installation',
-        message: 'How does one install your project?'
+        name: 'contribute',
+        message: 'How can others contribute to this project?',
+        when: ({contributingConfirm}) => contributingConfirm
     },
-{
-    type: 'input',
-    name: 'usage',
-    message: 'How is this application used?'
-},
-{
-    type: 'confirm',
-    name: 'screenshotConfirm',
-    message: 'Would you like to include screenshot(s)?',
-    default: false
-},
-{
-    type: 'link',
-    name: 'screenshot',
-    message: 'Enter the link to your screenshot.'
-},
-{
-    type: 'confirm',
-    name: 'screenshotConfirmTwo',
-    message: 'Would you like to include another screenshot?',
-    default: false
-},
-{
-type: 'input',
-name: 'credits',
-message: 'Who worked on this project and needs to be listed in the credit section?'
-},
-{
-    type: 'link',
-    name: 'creditLink',
-    message: 'Please enter the GitHub link for your collaborator.'
-},
-{
-    type: 'input',
-    name: 'license',
-    message: 'What license does this project hold?'
-},
-{
-    type: 'confirm',
-    name: 'badgesConfirm',
-    message: 'Would you like to list any badges?',
-    default: false
-},
-{
-    type: 'input',
-    name: 'badges',
-    message: 'What badges would you like to add?'
-},
-{
-    type: 'confirm',
-    name:'contributingConfirm',
-    message: 'Would you like others to contribute to this project?',
-    default: false
-},
-{
-    type: 'input',
-    name: 'contribute',
-    message: 'How can others contribute to this project?'
-},
-{
-    type: 'confirm',
-    name: 'testConfirm',
-    message: 'Would you like to provide any test examples for your project?',
-    default: false
-},
-{
-    type: 'input',
-    name: 'test',
-    message: 'Please provide test examples for this project.'
-}
-])
+    {
+        type: 'confirm',
+        name: 'testConfirm',
+        message: 'Would you like to provide any test examples for your project?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test examples for this project.',
+        when: ({testConfirm}) => testConfirm
+    }
+
+];
+
+
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then(function(data) {
+// function to write README file
+fs.writeFile('README.md', generateMarkdown(data), err => {
+    if (err) throw err;
+    console.log("File has been generated.");
+    });
+    
+    })
+
 };
-promptUser().then(answers => console.log(answers));
+
+
+
+
+
 
 // function call to initialize program
-// init();
-// promptUser();
+init();
